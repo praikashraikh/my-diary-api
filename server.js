7abbe52f0fcf5af044791874ac4cc62130509811
prai <prai@hubgroup.com>
2022-06-17 23:56:29 -0500

    Query and parms

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 
 const { MongoClient } = require('mongodb');
@@ -9,14 +10,33 @@ const client = new MongoClient(connectionString);
 const dbName = 'MyDiaries';
 
 async function getDiaries(req, res) {
+  const searchParam = req.query.search;
+  const abc = req.query.test;
+  console.log("Search params found: ", searchParam);
+  console.log("abc: ", abc);
+
   // Use connect method to connect to the server
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection('Diaries');
-  const result = await collection.find({}).toArray();
+  const result = await collection.find({ "title": searchParam }).toArray();
   return res.json(result);
 }
 
 app.get('/diaries', getDiaries);
+  
+async function getDiary(req, res) {
+  const id = req.params.id;
+  const query = { _id: Number(id) };
+  console.log("Query: ", query);
+
+  await client.connect();
+  const db = client.db(dbName);
+  const collection = db.collection('Diaries');
+  const result = await collection.find(query).toArray();
+  return res.json(result);
+}
+
+app.get('/diary/:id', getDiary);
 
 app.listen(3000);
